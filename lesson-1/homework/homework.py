2cb8a1ff3970f6f03f4490ef4e403cfcfaa236dd
Amriddin123 <tredandtred@gmail.com>
2025-10-14 20:53:36 +0500

    Create homework.py

---
+++ lesson-1/homework/homework.py
@@ -0,0 +1,33 @@
+# 1-task
+# a = float(input("Kvadratning tomon uzunligini kiriting"))
+
+# perimetiri = a * 4 
+# maydoni  = a ** 2
+# print("maydoni" ,perimetiri )
+# print("maydoni ", maydoni)
+
+# 3-task
+# a = float(input("birinchi sonni kiriting"))
+# b = float(input("ikkinchi sonni kiriting"))
+
+# sum = (a + b) / 2
+# print(" ikki sonni ortacha qiymati " ,sum)
+
+# 4-task 
+# a= float(input("birinchi son"))
+# b = float(input("ikkinchi son"))  
+
+# yigindi = a + b
+# kopaytma = a * b
+# kv_a = a ** 2
+# kv_b = b** 2
+# print("qoshuv",yigindi )
+# print("kopaytiriuv",kopaytma)
+# print("daraja",a ,kv_a)
+# print("daraja", b,kv_b)
+
+
+# import math
+# d=float(input('diametr kiriting'))
+# l=math.pi*d
+# print(f"doira uzunligi {l}")
