e50745d13d5e5fe7e8fc6a0d15b5434ebcb87e84
Amriddin123 <tredandtred@gmail.com>
2025-10-23 18:06:37 +0500

    Create homework.py

---
+++ lesson-2/homework/homework.py
@@ -0,0 +1,59 @@
+# Foydalanuvchining ismi va tug'ilgan yilini so'rash uchun Python dasturini yozing, so'ngra ularning yoshini hisoblang va ko'rsating.
+# 1-task
+# name = input("ismingizni kiriting  ")
+# age = int(input("tug'ilgan yilizni kiriting "))
+
+# print(f'salom {name}siz {2024 - age} yoshdasiz')
+
+# Quyidagi matndan avtomobil nomlarini ajratib oling:
+# 2-task
+# txt = 'LMaasleitbtui'
+# print(txt[::2])
+# print(txt[1:: 2 ])
+
+# Quyidagi matndan avtomobil nomlarini ajratib oling:
+# 3-task
+# txt = 'MsaatmiazD'
+# print(txt[:: 2])
+# damas =  txt[::-1]
+# print(damas[::2])
+
+
+# Quyidagi matndan yashash joyini ajratib oling:
+# 4--task
+# txt = "I'am John. I am from London"
+# print(txt[20 : ])
+
+# Foydalanuvchi kiritish satrini oladigan va uni teskari tartibda chop etadigan Python dasturini yozing.
+# 5--task
+# text = input("biror satir kiriting ")
+# print(text[::-1])
+
+
+# Berilgan qatordagi unlilar sonini hisoblaydigan Python dasturini yozing.
+# # 6-task
+# txt = input("nimadur yozing ")
+# a= txt.count("a")
+# u = txt.count("u")
+# e = txt.count("e")
+# i = txt.count("i")
+# o =txt.count("o")
+# print(a+u+e+i+o)
+
+
+# Raqamlar ro'yxatini kirish sifatida qabul qiladigan va maksimal qiymatni chop etadigan Python dasturini yozing.
+# 7-task
+# num = [1,2,3,5,10,45]
+# print(max(num))
+
+# Berilgan so'z palindrom ekanligini tekshiradigan Python dasturini yozing (bir xil oldinga va orqaga o'qiydi).
+# # 8-task
+# txt = 'aziza'
+# a = txt[::-1]
+# print(f'bu soz palindrom mi {txt == a}')
+
+# Foydalanuvchi tomonidan taqdim etilgan elektron pochta manzilidan domenni ajratib oladigan va chop etadigan Python dasturini yozing.
+# 9-task
+# gmail = input("gmailizni kiriting  ")
+# domen = gmail.split("@")[1]
+# print(f'sizning domenigiz {domen}')
